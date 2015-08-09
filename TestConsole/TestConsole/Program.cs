34a1c70fe20f3c318ed2e6a87b892f010a7efa5e
José Manuel Nieto Sánchez <superjmn@outlook.com>
2015-08-09 16:30:51 +0200

    Shitty feature

--- TestConsole/TestConsole/Program.cs
+++ TestConsole/TestConsole/Program.cs
@@ -1,9 +1,18 @@
 ﻿namespace TestConsole
 {
+    using System;
+    using System.Diagnostics;
+
     class Program
     {
         static void Main(string[] args)
         {
+            Shitty();
+        }
+
+        public static void Shitty()
+        {            
+            Console.WriteLine("");
         }
     }
 }
